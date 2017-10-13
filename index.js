6c74d02058e0ca78babb03a838a4451aad7d6936
Crista Cooper <aine3d@gmail.com>
2017-10-13 17:25:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine) {
     return("The line is currently empty.");
   } else {
     for (var i = 0; i < katzDeliLine.length; i++) {
-      newLine.push(" " + (i + 1) + ". " + katzDeliLine[i]);
+      newLine.push((i + 1) + ". " + katzDeliLine[i]);
     }
     return ("The line is currently: " + newLine );
   }
