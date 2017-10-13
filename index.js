3d657934b9e87ebbbb05bbe8ba8411244dbb89b7
Crista Cooper <aine3d@gmail.com>
2017-10-13 16:46:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function takeANumber(katzDeliLine, name) {
     katzDeliLine.push(name);
     return ("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.")
 }
+
+function nowServing() {
+  if (katzDeliLine === 0) {
+    return ("There is nobody waiting to be served")
+  } else {
+    katzDeliLine.shift(name)
+  }
+}
