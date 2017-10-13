e94c4845f52332a219d13d2b8074987762b3fad7
Crista Cooper <aine3d@gmail.com>
2017-10-13 16:23:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var i = 0;
 function takeANumber(katzDeliLine, name) {
     var position = katzDeli.length + 1;
     katzDeli.push(name);
-    return ("Welcome, ${name}. You are number " + position + " in line.")
+    return (`Welcome, ${name}. You are number ` + position + " in line.")
 }
