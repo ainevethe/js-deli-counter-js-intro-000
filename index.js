7230627471a988df32a45cf56d3447f4e371acd7
Crista Cooper <aine3d@gmail.com>
2017-10-13 17:13:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function currentLine(katzDeliLine) {
     return("The line is currently empty.")
   } else {
     for (var i = 0; i < katzDeliLine.length; i++) {
+      var place = [i]+1;
     return("The line is currently: " + [i+1] + katzDeliLine[i])
   }
 }
