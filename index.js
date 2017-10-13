ee7036809b7345792853c98a3f15ccc0dedf10da
Crista Cooper <aine3d@gmail.com>
2017-10-13 16:05:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var i = 0;
 function takeANumber(katzDeliLine, name) {
   for (i = 0; i < katzDeliLine.length; i++) {
     katzDeli.push(name[i]);
-    return ("Welcome, " + name + ". You are number " + katzDeliLine + " in line.")
+    return ("Welcome, " + katzDeliLine[i] + ". You are number " +  + " in line.")
   }
   return takeANumber;
 }
