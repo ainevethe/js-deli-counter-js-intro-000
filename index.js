3a3da42446a79ebeb2fe6a2f405d6289c5aab113
Crista Cooper <aine3d@gmail.com>
2017-10-13 17:23:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine(katzDeliLine) {
   } else {
     for (var i = 0; i < katzDeliLine.length; i++) {
       newLine.push(" " + (i + 1) + ". " + katzDeliLine[i])
-    return "The line is currently: " + newLine
+    return ("The line is currently: " + newLine );
   }
 }
 }
