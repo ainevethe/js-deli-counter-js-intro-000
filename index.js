e0c31ec99193065afb25f99483837b0df3bedc65
Crista Cooper <aine3d@gmail.com>
2017-10-13 15:59:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+
+var katzDeli = [];
+var i = 0;
+
+function takeANumber(katzDeliLine, name) {
+  for (i = 0; i < katzDeliLine.length; i++) {
+    return (${name} + ", you are currently number " + katzDeliLine[i] + " in line.")
+  }
+}
