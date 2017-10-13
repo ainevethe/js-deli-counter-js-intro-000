8e4b6bed4cbd3931137051de9e7e7a708bfceb91
Crista Cooper <aine3d@gmail.com>
2017-10-13 16:52:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine, name) {
 
-  if (katzDeliLine === 0) {
+  if (katzDeli === 0) {
     return ("There is nobody waiting to be served!")
   } else {
     katzDeli.shift(name)
